40bb3320057e3e42679f8ae2fe5d13fa8ad8c353
Kuuipoo <kuuipo@gmail.com>
2019-11-20 08:57:01 +0700

    edit

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -5,7 +5,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 const { WebhookClient, Payload } = require('dialogflow-fulfillment');
+const ChangeValue = nameDay => {
+  let _day = nameDay.toLowerCase();
+  let _data;
+  if (_day === 'sun' || _day === 'sunday' || _day === 'อาทิตย์') {
+    _data = 'Sunday';
+  } else if (_day === 'mon' || _day === 'monday' || _day === 'จันทร์') {
+    _data = 'Monday';
+  } else if (_day === 'tue' || _day === 'tuesday' || _day === 'อังคาร') {
+    _data = 'Tuesday';
+  } else if (_day === 'wed' || _day === 'wednesday' || _day === 'พุธ') {
+    _data = 'Wednesday';
+  } else if (_day === 'thu' || _day === 'thursday' || _day === 'พฤหัสบดี') {
+    _data = 'Thursday';
+  } else if (_day === 'fri' || _day === 'friday' || _day === 'ศุกร์') {
+    _data = 'Friday';
+  } else if (_day === 'sat' || _day === 'saturday' || _day === 'เสาร์') {
+    _data = 'Saturday';
+  }
 
+  return _data;
+};
 const tempLine = () => {
   const temp = {
     type: 'flex',
@@ -69,43 +89,45 @@ const chooseDay = nameDay => {
   let titleMoney = 'สีมงคลเสริมดวงการเงิน';
   let titleWork = 'สีมงคลเสริมดวงการงาน';
   let titleLove = 'สีมงคลเสริมดวงความรัก';
-  if (nameDay === 'Sunday') {
+  let _day = ChangeValue(nameDay);
+
+  if (_day === 'Sunday') {
     data = {
       dtlMoney: 'ดำ ม่วง',
       dtlWork: 'แดง',
       dtlLove: 'เขียว เทา ทอง'
     };
-  } else if (nameDay === 'Mondey') {
+  } else if (_day === 'Monday') {
     data = {
       dtlMoney: 'ส้ม เหลืองแก่',
       dtlWork: 'ชมพู เขียว ฟ้า',
       dtlLove: 'น้ำเงิน ทอง'
     };
-  } else if (nameDay === 'Tuesday') {
+  } else if (_day === 'Tuesday') {
     data = {
       dtlMoney: 'น้ำตาล',
       dtlWork: 'ม่วงแก่ ดำ เทา แดง',
       dtlLove: 'เขีียว ชมพู'
     };
-  } else if (nameDay === 'Wednesday') {
+  } else if (_day === 'Wednesday') {
     data = {
       dtlMoney: 'ม่วงแก่',
       dtlWork: 'น้ำตาล ทอง ขาว เหลืองอ่อน น้ำเงิน ฟ้า',
       dtlLove: 'เขียว ดำ'
     };
-  } else if (nameDay === 'Thursday') {
+  } else if (_day === 'Thursday') {
     data = {
       dtlMoney: 'ฟ้า ทอง',
       dtlWork: 'เขียว ขาว',
       dtlLove: 'เทา บรอนซ์ ส้ม'
     };
-  } else if (nameDay === 'Friday') {
+  } else if (_day === 'Friday') {
     data = {
       dtlMoney: 'น้ำตาล ฟ้า น้ำเงิน',
       dtlWork: 'ทอง เหลือง',
       dtlLove: 'ดำ เขียว'
     };
-  } else if (nameDay === 'Saturday') {
+  } else if (_day === 'Saturday') {
     data = {
       dtlMoney: 'เทา บรอนซ์',
       dtlWork: 'ชมพู ทอง เหลือง',
@@ -122,43 +144,45 @@ const chooseDay = nameDay => {
 };
 module.exports.webhook = async app => {
   app.post('/webhook', (req, res) => {
-    const agent = new WebhookClient({
-      request: req,
-      response: res
-    });
-    let _data = tempLine();
-    let payload = new Payload(`LINE`, _data, { sendAsMessage: true });
+    try {
+      const agent = new WebhookClient({
+        request: req,
+        response: res
+      });
+      let _data = tempLine();
+      let payload = new Payload(`LINE`, _data, { sendAsMessage: true });
 
-    function color(agent) {
-      const nameDay = req.body.queryResult.parameters.any;
-      let data = chooseDay(nameDay);
-      if (data) {
-        agent.add(req.body.queryResult.fulfillmentText);
-        agent.add(data.dtlMoney);
-        agent.add(data.dtlWork);
-        agent.add(data.dtlLove);
-      } else {
-        agent.add(req.body.queryResult.fulfillmentText);
-        agent.add(payload);
+      function color(agent) {
+        const nameDay = req.body.queryResult.parameters.any;
+        let data = chooseDay(nameDay);
+        if (data) {
+          agent.add(data.dtlMoney);
+          agent.add(data.dtlWork);
+          agent.add(data.dtlLove);
+        } else {
+          agent.add(req.body.queryResult.fulfillmentText);
+          agent.add(payload);
+        }
       }
-    }
 
-    function custom(agent) {
-      const nameDay = req.body.queryResult.parameters.nameday;
-      let data = chooseDay(nameDay);
-      if (data) {
-        agent.add(req.body.queryResult.fulfillmentText);
-        agent.add(data.dtlMoney);
-        agent.add(data.dtlWork);
-        agent.add(data.dtlLove);
-      } else {
-        agent.add('ไม่อยากเลือกหรอก เสียใจน้า');
+      function custom(agent) {
+        const nameDay = req.body.queryResult.parameters.date;
+        let data = chooseDay(nameDay);
+        if (data) {
+          agent.add(data.dtlMoney);
+          agent.add(data.dtlWork);
+          agent.add(data.dtlLove);
+        } else {
+          agent.add(req.body.queryResult.fulfillmentText);
+        }
       }
-    }
-    let intentMap = new Map();
-    intentMap.set('Auspicious color', color);
-    intentMap.set('Auspicious color - custom', custom);
+      let intentMap = new Map();
+      intentMap.set('Auspicious color', color);
+      intentMap.set('Auspicious color - custom', custom);
 
-    agent.handleRequest(intentMap);
+      agent.handleRequest(intentMap);
+    } catch (error) {
+      console.error(error.toString());
+    }
   });
 };
